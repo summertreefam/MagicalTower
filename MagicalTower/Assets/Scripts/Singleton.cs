ca4dfd8e1737a7204bffbed9c324053806250020
dennis <dennis@vespainteractive.com>
2019-06-25 12:12:10 +0900

    singleton 추가

---
+++ MagicalTower/Assets/Scripts/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NSingleton
+{
+    public class Singleton
+        : MonoBehaviour
+    {
+        static Singleton _singleton = null;
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if(_singleton == null)
+                {
+                    _singleton = new Singleton();
+                }
+
+                return _singleton;
+            }
+        }
+    }
+}
+
